653ee3d147ed9030fc4c343bddf168af3794c14b
Ryan D <ryancdavey1@gmail.com>
2020-05-11 16:37:42 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,13 @@
 def oxford_comma(array)
   new_array = []
   array.map.with_index do|word, i|
-    i == array.length - 1 ? new_array << "and " + word : new_array << word + ","
+    if array.length == 0 || array.length == 1
+      new_array << word
+    elsif array.length == 2
+      new_array = array.join(" and ")
+    else 
+      i == array.length - 1 ? new_array << "and " + word : new_array << word + ","
+    end
   end
   new_array.join
 end
