14c019fc62f7393d0f65c77fe28505dd8e75af3a
Ryan D <ryancdavey1@gmail.com>
2020-05-11 16:43:01 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
 def oxford_comma(array)
-
+  new_array = []
+  if array.length == 0 || array.length == 1
+    new_array = array.join()
+  elsif array.length == 2
+    new_array = array.join(" and ")
+  else  
+    array.map.with_index do|word, i|
+      if i == array.length - 1 
+        new_array << "and " + word 
+      else
+        new_array << word + ", "
+      end
+    end
+    new_array.join
+  end
 end
